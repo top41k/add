aead5d30510e57e50f69e0c223696268bec57847
top41k <57314570+top41k@users.noreply.github.com>
2021-09-11 12:58:31 +0500

    test
    tesst

---
+++ build/precache-manifest.1c24e8f316bced10e8ecebfa53068fb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c3f0a82b8fa1491e6b473c323223030",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b3e06535e0ebc08e21dd",
+    "url": "/static/css/2.cdee455b.chunk.css"
+  },
+  {
+    "revision": "74d2e1dcf53b059f532c",
+    "url": "/static/css/main.9da65429.chunk.css"
+  },
+  {
+    "revision": "b3e06535e0ebc08e21dd",
+    "url": "/static/js/2.efd26eb6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.efd26eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74d2e1dcf53b059f532c",
+    "url": "/static/js/main.d78c5f8e.chunk.js"
+  },
+  {
+    "revision": "8f3f64d0671efe8159c3",
+    "url": "/static/js/runtime-main.3807a136.js"
+  }
+]);
